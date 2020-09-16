2283aec25cdb3475e1c3d3d4e7bd5b1fe6ba728b
Daniil-Auto <70564934+Daniil-Auto@users.noreply.github.com>
2020-09-16 19:42:01 +0700

    Page object add elelment see

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     #Конструктор - метод. Передаём экземпляр драйвера и url адрес.
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-    
+        self.browser.implicitly_wait(timeout)
+        
     #Открываем нужную страницу в браузере
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
