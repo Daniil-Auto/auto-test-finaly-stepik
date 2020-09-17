a3f44f338c2da16c1bb2403078b15f5e978aa7ff
Daniil-Auto <70564934+Daniil-Auto@users.noreply.github.com>
2020-09-17 17:39:24 +0700

    Page object add locators, add Login_page

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        link_login_page = self.current_url
+        index = link_login_page.find('login')
+        if index !=1:
+            print('URL Correct')
+        assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.FORM_LOGIN), "Login form is not presented"
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.FORM_REGISTER), "Login register is not presented"
+        assert True
